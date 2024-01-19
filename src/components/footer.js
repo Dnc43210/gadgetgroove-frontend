68a3ff5aa90d88875b10c81b4479208992996c7c
Rishi1402 <rishi_rai@persistent.com>
2024-01-19 14:49:53 +0530

    added footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -36,7 +36,7 @@ export function Footer() {
                 {links.map((link, key) => (
                   <Typography key={key} as="li" color="blue-gray" className="font-normal">
                     <a
-                      href="#"
+                      href="/"
                       className="inline-block py-1 pr-2 transition-transform hover:scale-105"
                     >
                       {link}
@@ -52,7 +52,7 @@ export function Footer() {
             variant="small"
             className="mb-4 text-center font-normal text-blue-gray-900 md:mb-0"
           >
-            &copy; {currentYear} <a href="https://material-tailwind.com/">Material Tailwind</a>. All
+            &copy; {currentYear} <a href="https://material-tailwind.com/">Gadget Groove</a>. All
             Rights Reserved.
           </Typography>
           <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
