273a379937efd3fd78eed1810af30482a169f27f
Dnc43210 <deep_chaudhuri@persistent.com>
2024-01-30 10:27:10 +0530

    implemented search and minor changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-
 import Collections from "./components/Collections";
 import NavigationBar from "./components/NavigationBar";
-
 import React, { Component } from "react";
-import Signin from "./components/Signin";
 import { Footer } from "./components/Footer";
 import Categories from "./components/Categories";
 import SearchPage from "./components/SearchPage";
@@ -14,33 +11,23 @@ import ProductList from "./components/ProductList";
 // const withMT = require("@material-tailwind/react/utils/withMT");
 
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isLogged: true,
-    };
-  }
-
   render() {
     return (
       <>
-        {this.state.isLogged ? (
-          <div className="Home">
+        <div className="Home">
             <NavigationBar />
-            <Routes>
-              <Route path="/" element={<Collections />}></Route>
-              <Route path="/home" element={<Collections />}></Route>
-              <Route path="/categories" element={<Categories />}></Route>
-              <Route path="/category/:categoryName" element={<ProductList/>}></Route>
-              <Route path="/search" element={<SearchPage />}></Route>
-            </Routes>
+          <Routes>
+            <Route path="/" element={<Collections />}></Route>
+            <Route path="/home" element={<Collections />}></Route>
+            <Route path="/categories" element={<Categories />}></Route>
+            <Route
+              path="/category/:categoryName"
+              element={<ProductList />}
+            ></Route>
+            <Route path="/search" element={<SearchPage />}></Route>
+          </Routes>
             <Footer />
-          </div>
-        ) : (
-          <>
-            <Signin />
-          </>
-        )}
+        </div>
       </>
     );
   }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import ProductCard from "./ProductCard";
 // import axios from "axios";
 
 export default function ProductList() {
@@ -12,9 +13,7 @@ export default function ProductList() {
       .then((data) => setProducts(data.products))
       .catch((error) => console.error("error fetching categories", error));
     // console.log(products)
-  }, []);
-
-  // console.log(products)
+  }, [categoryName]);
 
   return (
     <div className="bg-white">
@@ -26,50 +25,7 @@ export default function ProductList() {
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {products &&
             products.map((product, i) => (
-              <Link className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-                <a href={product.href} key={"product" + 1}>
-                  <img
-                    src={product.images[0]}
-                    alt={product.imageAlt}
-                    className="h-80 w-72 object-cover rounded-t-xl"
-                  />
-                  <div className="px-4 py-3 w-72">
-                    <span className="text-gray-400 mr-3 uppercase text-xs">
-                      {product.brand}
-                    </span>
-                    <p className="text-lg font-bold text-black truncate block capitalize">
-                      {product.title}
-                    </p>
-                    <div className="flex items-center">
-                      <p className="text-lg font-semibold text-black cursor-auto my-3">
-                        {"$" + product.price}
-                      </p>
-                      <del>
-                        <p className="text-sm text-gray-600 cursor-auto ml-2">
-                          $199
-                        </p>
-                      </del>
-                      <div className="flex items-center ml-auto bg-orange-500 px-3 py-3 rounded-2xl text-gray-100 ">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="20"
-                          height="20"
-                          fill="currentColor"
-                          className="bi bi-bag-plus"
-                          viewBox="0 0 16 16"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
-                          />
-                          <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                        </svg>
-                        &nbsp;&nbsp;Add to Cart
-                      </div> 
-                    </div>
-                  </div>
-                </a>
-              </Link>
+              <ProductCard product={product}/>
             ))}
         </div>
       </div>
