cd30cb9f80b50af5d7eee1f7c5e6e474649339fd
Dnc43210 <deep_chaudhuri@persistent.com>
2024-02-07 23:15:45 +0530

    Revert "modified cartpage, started with chatbot"
    This reverts commit e17ccd2992b2f3b626b93b2323b302aa481bbc2d.

--- src/Layout.js
+++ src/Layout.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import NavigationBar from './components/NavigationBar'
 import { Footer } from './components/Footer'
-import ChatBotPanel from './components/ChatBotPanel';
 
 function Layout({children}) {
   return (
     <div>
       <NavigationBar/>
       {children}
-      <ChatBotPanel />
       <Footer/>
     </div>
   )
